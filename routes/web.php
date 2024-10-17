3de0206de96395ccf1f09f91d6ec34b4dc19dd01
Dulce1203 <dulcecaribran56@gmail.com>
2024-10-16 18:32:38 -0600

    Configuración de rutas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// routes/web.php
+Route::get('/admin', 'AdminController@index')->name('admin.index');
+Route::get('/admin/users', 'AdminController@users')->name('admin.users');
+Route::get('/admin/settings', 'AdminController@settings')->name('admin.settings');
