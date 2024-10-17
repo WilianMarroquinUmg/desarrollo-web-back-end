7123407d4b3163b7b70f870ee0de425eb59bf82e
Dulce1203 <dulcecaribran56@gmail.com>
2024-10-16 19:11:13 -0600

    Creación de vistas- Menú, Usuarios, configuraciones

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Panel de Administración</h1>
+    <p>Bienvenido al panel de administración. Elige una opción del menú.</p>
+@endsection
---
+++ resources/views/admin/settings.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Configuración</h1>
+    <p>Ajusta las configuraciones del sistema.</p>
+@endsection
