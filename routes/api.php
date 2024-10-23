77747f12e4710d46264fb42f2f2c65a2e0e595e6
WilianMarroquin <githubwiliangit@gmail.com>
2024-10-22 18:18:29 -0600

    Correccion en rutas para que servidor pueda interpretarlas

--- routes/api.php
+++ routes/api.php
@@ -9,33 +9,33 @@ Route::get('/user', function (Request $request) {
 
 
 Route::prefix('auth')->group(function () {
-    Route::post('signup', 'App\Http\Controllers\Api\Auth\AuthApiController@signup')->name('auth.signup');
-    Route::post('login', 'App\Http\Controllers\Api\Auth\AuthApiController@login')->name('auth.login');
-    Route::post('logout', 'App\Http\Controllers\Api\Auth\AuthApiController@logout')->middleware('auth:sanctum')->name('auth.logout');
-    Route::get('user', 'App\Http\Controllers\Api\Auth\AuthApiController@getAuthenticatedUser')->middleware('auth:sanctum')->name('auth.user');
+    Route::post('signup', 'App\Http\Controllers\API\Auth\AuthApiController@signup')->name('auth.signup');
+    Route::post('login', 'App\Http\Controllers\API\Auth\AuthApiController@login')->name('auth.login');
+    Route::post('logout', 'App\Http\Controllers\API\Auth\AuthApiController@logout')->middleware('auth:sanctum')->name('auth.logout');
+    Route::get('user', 'App\Http\Controllers\API\Auth\AuthApiController@getAuthenticatedUser')->middleware('auth:sanctum')->name('auth.user');
 
-    Route::post('/password/email', 'App\Http\Controllers\Api\Auth\AuthApiController@sendPasswordResetLinkEmail')->middleware('throttle:5,1')->name('password.email');
-    Route::post('/password/reset', 'App\Http\Controllers\Api\Auth\AuthApiController@resetPassword')->name('password.reset');
+    Route::post('/password/email', 'App\Http\Controllers\API\Auth\AuthApiController@sendPasswordResetLinkEmail')->middleware('throttle:5,1')->name('password.email');
+    Route::post('/password/reset', 'App\Http\Controllers\API\Auth\AuthApiController@resetPassword')->name('password.reset');
 });
 
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
 
-    Route::apiResource('users', 'App\Http\Controllers\Api\UserApiController');
+    Route::apiResource('users', 'App\Http\Controllers\API\UserApiController');
 
-    Route::apiResource('direcciones', 'App\Http\Controllers\Api\DireccionApiController');
+    Route::apiResource('direcciones', 'App\Http\Controllers\API\DireccionApiController');
 
-    Route::apiResource('residentes', 'App\Http\Controllers\Api\ResidenteApiController');
+    Route::apiResource('residentes', 'App\Http\Controllers\API\ResidenteApiController');
 
-    Route::apiResource('tipos-adquisicions', 'App\Http\Controllers\Api\TipoAdquisicionApiController');
+    Route::apiResource('tipos-adquisicions', 'App\Http\Controllers\API\TipoAdquisicionApiController');
 
-    Route::apiResource('paja-aguas', 'App\Http\Controllers\Api\PajaAguaApiController');
+    Route::apiResource('paja-aguas', 'App\Http\Controllers\API\PajaAguaApiController');
 
-    Route::get('paja-aguas/getDetalles/residente{id}', 'App\Http\Controllers\Api\PajaAguaApiController@getDetallesForResidente');
+    Route::get('paja-aguas/getDetalles/residente{id}', 'App\Http\Controllers\API\PajaAguaApiController@getDetallesForResidente');
 
-    Route::get('paja-aguas/from/residente/{id}', 'App\Http\Controllers\Api\PajaAguaApiController@getPajaAguasFromResidente');
+    Route::get('paja-aguas/from/residente/{id}', 'App\Http\Controllers\API\PajaAguaApiController@getPajaAguasFromResidente');
 
-    Route::post('traslados-paja-agua', 'App\Http\Controllers\Api\PajaAguaApiController@trasladarResidente');
+    Route::post('traslados-paja-agua', 'App\Http\Controllers\API\PajaAguaApiController@trasladarResidente');
 
 
 });
